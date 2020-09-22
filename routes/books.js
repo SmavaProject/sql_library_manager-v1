46d895b08d75ed2ae7d3235b155cd7a5e3534ef0
SmavaProject <katyakravchenka@yahoo.com>
2020-09-22 07:40:15 +0300

    get by id

--- routes/books.js
+++ routes/books.js
@@ -22,10 +22,22 @@ router.get('/', asyncHandler(async (req, res, next) => {
 }));
 
 /* GET form for creating a new book */
-router.get('/books/new', asyncHandler(async (req, res) => {
-    res.render('new-book', {book: {}, title: ""});
+router.get('/new', asyncHandler(async (req, res) => {
+    res.render('new-book', {book: Book.build(), title: ""});
 }));
 
 /*POST - create a new book*/
+router.post('/new', asyncHandler((req, res, next) => { // / rounte??
+    Book.create().then(function (book) {
+        res.redirect('/books/' + book.id); //+ book.id
+    });
+}));
+
+/* GET books by id*/
+router.get('/:id', asyncHandler((req, res, next) => {
+    Book.findById(req.param.id).then(function(book){
+        res.render('update-book', {book: book, title: book.title});
+    })
+}));
 
 module.exports = router;
